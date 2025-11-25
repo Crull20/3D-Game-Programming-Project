0b4794b1dd91d9507cf4a02d68d1d9e37a1bea75
Diego Vasquez <145511664+DiegoVDM@users.noreply.github.com>
2025-11-25 14:28:42 -0800

    Added two packets, a terrain, and a basic movement
    Two packets added are named Starter Assets and Polytope Studio. Terrain has texture and some hills, mountains can be added. And added a basic cylinder character model with basic movement(walk, jump, sprint, look with mouse). Can test walk around map by pressing play.

---
+++ Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+    Vector3 inputDir;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        inputDir = new Vector3(h, 0f, v).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetVelocity = inputDir * moveSpeed;
+        Vector3 velocity = targetVelocity;
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;
+    }
+}
